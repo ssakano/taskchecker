a1292931be03180fca320bff2b460566c6304290
ssakano <48211591+ssakano@users.noreply.github.com>
2019-06-21 04:12:42 +0000

    task model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+# ApplicationRecordはさらにActiveRecord::Baseを継承している。
+# ActiveRecordは、RailsのModel全般の操作を行うライブラリのこと。
---
+++ app/models/task.rb
@@ -0,0 +1,3 @@
+class Task < ApplicationRecord  # TaskクラスはApplicationRecordクラスを継承している。
+                                # ApplicationRecordクラスが定義されたファイルはapp/models/application_record.rb
+end
