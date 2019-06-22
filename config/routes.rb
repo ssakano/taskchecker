2f219dd50bbab46ac706c5d5d619cd0975349526
ssakano <48211591+ssakano@users.noreply.github.com>
2019-06-22 13:15:08 +0000

    admin_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+
+
    #トップページ(/)にアクセスした時、tasksコントローラのindexアクションに処理を流す。
   root to: 'tasks#index'
-  
   resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190621020814) do
+ActiveRecord::Schema.define(version: 20190622094936) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "task"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20190621020814) do
     t.string   "status"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false, null: false
   end
 
 end
